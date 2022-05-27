27a6a56054ee94e4507557e3379df225fd1985bf
Tolgahan Karabudak <67536491+tolga-karabudak@users.noreply.github.com>
2022-05-27 18:00:14 +0300

    deleting sql

--- sql-server/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#start SQL Server, start the script to create the DB and import the data
-/usr/init_db/import-data.sh &
-/opt/mssql/bin/sqlservr
--- sql-server/init-sql-scripts/setup.sql
+++
@@ -1,2 +0,0 @@
-:r /usr/init_db/init-sql-scripts/01_create_database.sql 
-:r /usr/init_db/init-sql-scripts/02_create_tables.sql
