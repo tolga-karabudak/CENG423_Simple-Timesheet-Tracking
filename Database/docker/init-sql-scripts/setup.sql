03573b52cb8fba73cdfd22a2ca47456bbde65c87
@kaanabb <kaan@LAPTOP-0TLP0MB7.localdomain>
2022-06-20 18:46:17 +0300

    db-connection-check

---
+++ Database/docker/init-sql-scripts/setup.sql
@@ -0,0 +1,3 @@
+:r /usr/init_db/init-sql-scripts/01_create_database.sql
+:r /usr/init_db/init-sql-scripts/02_create_schemas.sql
+:r /usr/init_db/init-sql-scripts/03_create_tables.sql
--- TaskManagement_with_API/TimeSheet/Models/Context.cs
+++ TaskManagement_with_API/TimeSheet/Models/Context.cs
@@ -10,7 +10,8 @@ namespace TimeSheet.Models
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("workstation id=aybavtr.mssql.somee.com;packet size=4096;user id=noctustest1_SQLLogin_1;pwd=nfyhx8fn54;data source=aybavtr.mssql.somee.com;persist security info=False;initial catalog=aybavtr");
+            //optionsBuilder.UseSqlServer("workstation id=aybavtr.mssql.somee.com;packet size=4096;user id=noctustest1_SQLLogin_1;pwd=nfyhx8fn54;data source=aybavtr.mssql.somee.com;persist security info=False;initial catalog=aybavtr");
+		optionsBuilder.UseSqlServer("Data Source = localhost; Initial Catalog = Timesheet; Persist Security Info = True; User ID = sa; Password = Timesheet2022; Encrypt = False");
         }
 
 
